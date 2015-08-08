5fd8974746675ad1df4ca9f8f6dcd548db532ca5
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-08-08 14:26:13 -0700

    scope api

--- app/backend/config/routes.rb
+++ app/backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :meetups
+  scope :api do
+    resources :meetups
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
