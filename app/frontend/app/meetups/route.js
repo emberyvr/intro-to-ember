a25907090cb5bc074e7a06cda67e5583771d3353
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-08-09 11:24:01 -0700

    Save meetup

--- app/frontend/app/meetups/route.js
+++ app/frontend/app/meetups/route.js
@@ -4,6 +4,18 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('meetup');
+  },
+
+  actions: {
+
+    saveMeetup(meetup) {
+      meetup.save().then(() => {
+        console.info('saved!')
+      }, () => {
+        console.error('failed to save!');
+      });
+    }
+
   }
 
 });
