339f911ca775d1477c336a5c8712ce0f77ed6c0d
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-08-09 12:02:33 -0700

    Register content types correctly so json-api works

--- app/backend/config/initializers/active_model_serializers.rb
+++ app/backend/config/initializers/active_model_serializers.rb
@@ -1,1 +1,10 @@
 ActiveModel::Serializer.config.adapter = ActiveModel::Serializer::Adapter::JsonApi
+
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
