ecce61103216942ea0a949fbc129c6ebb37808b0
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-08-09 18:21:07 -0700

    Adds home page and another computed props example

--- app/frontend/app/meetups/controller.js
+++ app/frontend/app/meetups/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  meetups: Ember.computed.alias('model')
+  meetups: Ember.computed.alias('model'),
+  firstMeetup: Ember.computed.readOnly('meetups.firstObject')
 
 });
